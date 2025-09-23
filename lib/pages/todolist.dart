e555f39b834eca5030f93c65dd2f2cf5b0d4fb07
Prabhat2056 <prabhat2056@gmail.com>
2025-09-23 17:26:55 +0545

    todolist  page

--- lib/pages/todolist.dart
+++ lib/pages/todolist.dart
@@ -2,7 +2,7 @@ import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter/material.dart";
 import "package:project_etb/pages/mylist.dart";
 
-import "package:project_etb/pages/todo.dart";
+import "package:project_etb/models/todo.dart";
 
 
 
